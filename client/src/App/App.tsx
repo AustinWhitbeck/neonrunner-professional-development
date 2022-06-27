fe83a12e9901d03f42bcb27bd835791a6be59e81
Austin Whitbeck <austin.whitbeck@millsteel.com>
2022-06-27 10:17:50 -0400

    added server/client file setup, getting TS setup

---
+++ client/src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../components/pages/login";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+};
+
+export default App;
