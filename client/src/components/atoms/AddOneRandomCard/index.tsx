d5e97a7c3f4383752f87e6ab243b6f473e35e8d2
Austin Whitbeck <austin.whitbeck@millsteel.com>
2022-07-11 17:32:08 -0400

    make list refresh on get add 1

--- client/src/components/atoms/AddOneRandomCard/index.tsx
+++ client/src/components/atoms/AddOneRandomCard/index.tsx
@@ -5,18 +5,18 @@ import { addOneRandomCardToUserCollection } from "../../../repository";
 
 interface Props {
   currentUser: User;
+  handleGetUserCollection: () => Promise<void>;
 }
 
-const AddOneRandomCard: React.FC<Props> = ({ currentUser }: Props) => {
-  return (
-    <Button
-      onClick={(): Promise<void> =>
-        addOneRandomCardToUserCollection(currentUser.user_id as number)
-      }
-    >
-      Add 1 Card
-    </Button>
-  );
+const AddOneRandomCard: React.FC<Props> = ({
+  currentUser,
+  handleGetUserCollection,
+}: Props) => {
+  const handleAddOne = (): void => {
+    addOneRandomCardToUserCollection(currentUser.user_id as number);
+    handleGetUserCollection;
+  };
+  return <Button onClick={handleAddOne}>Add 1 Card</Button>;
 };
 
 export default AddOneRandomCard;
